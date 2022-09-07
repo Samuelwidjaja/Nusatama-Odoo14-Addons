0b5410eaf528c95d3e62424137fbca5cb7ba137c
Imron Syabani <imoehsawa67@gmail.com>
2022-09-07 11:19:42 +0700

    update printout delivery order & printout bast

---
+++ nusatama_stock_report/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Stock Report",
+    'version': '1.0',
+    'depends': ['base','stock','account','sale_stock','purchase_stock'],
+    'author': "Alphasoft",
+    'category': 'Report',
+    'description': """
+        This module inherited report quotation
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/stock.xml',
+        'reports/report_stock_picking.xml',
+        'reports/report_stock_bast.xml',
+        'reports/report_action.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'images':[
+        'static/image/nusatama.png'
+        'static/image/table.jpg'
+    ]
+}
