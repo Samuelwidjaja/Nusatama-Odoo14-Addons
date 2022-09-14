4e1ed080f94aa4e5d8711b758b8fa7bfaefef0b8
ferdysaputra73 <ferdy_saputra73@gmail.com>
2022-09-14 11:42:33 +0700

    set default Mo from so

--- aos_draft_mo_so/__manifest__.py
+++ aos_draft_mo_so/__manifest__.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Aos Automation',
+    'name' : 'Aos Draft Mo From So',
     'version': '14.0.0.1.0',
     'license': 'OPL-1',
-    'summary': 'Aos Automation',
+    'summary': 'Aos Draft Mo From So',
     'sequence': 1,
     "author": "Alphasoft",
     'description': """
-Account Voucher
-====================
+        Aos Draft Mo From So'
     """,
     'category' : 'Tools',
     'website': 'https://www.alphasoft.co.id/',
     'images':  ['images/main_screenshot.png'],
     'depends' : ['mrp','stock', 'product'],
     'data': [
+        'views/mrp_inherited.xml',
     ],
     'demo': [],
     'test': [],
