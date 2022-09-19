09ac679e4f30b1a1e0626068c7d0605d733b9f3e
ferdysaputra73 <ferdy_saputra73@gmail.com>
2022-09-19 10:24:37 +0700

    update draft mo to so, user read

--- aos_draft_mo_so/models/mrp_automation.py
+++ aos_draft_mo_so/models/mrp_automation.py
@@ -1,34 +1,27 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
 
 
 class MrpAutomation(models.Model):
     _inherit = "mrp.production"
         
     confirm =fields.Boolean()
-    
     def action_confirm(self):
-        super(MrpAutomation,self).action_confirm()
         for production in self :
             if production['origin'] != False:
-                if production['state'] == 'confirmed' and production['confirm'] == False :
+                if production['state'] == 'draft' and production['confirm'] == False :
                     production['state'] = 'draft'
                     production['confirm'] = True
                 else :
-                    production['state'] = 'confirmed'
+                    super(MrpAutomation,self).action_confirm()
+            else :
+                    super(MrpAutomation,self).action_confirm()
 
-class StockMoves(models.Model):
-    _inherit = "stock.move"
+                    
+
+
+                    
+                
     
-    confirm =fields.Boolean()
-    
-    def _action_confirm(self):
-        super(StockMoves,self)._action_confirm()
-        for production in self :
-                if production['state'] == 'assigned' and production['confirm'] == False :
-                    production['state'] = 'confirmed'
-                    production['confirm'] == True
-                else :
-                    production['state'] = 'assigned'
 
         
         
