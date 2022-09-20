ad3e715a04d7d217664dd66a84b1dc61e40279af
ferdysaputra73 <ferdy_saputra73@gmail.com>
2022-09-20 12:30:19 +0700

    analytic from SO to Stock

---
+++ aos_analytic_stock_picking/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Aos Account Analytic So To Stock Picking',
+    'version': '14.0.0.1.0',
+    'license': 'OPL-1',
+    'summary': 'Aos Account Analytic So To Stock Picking',
+    'sequence': 1,
+    "author": "Alphasoft",
+    'description': """
+        Aos Account Analytic So To Stock Picking'
+    """,
+    'category' : 'Tools',
+    'website': 'https://www.alphasoft.co.id/',
+    'images':  ['images/main_screenshot.png'],
+    'depends' : ['mrp','aos_sales_auto_analytic', "mrp_analytic","stock"],
+    'data': [
+    ],
+    'demo': [],
+    'test': [],
+    'qweb': [],
+    'css': [],
+    'js': [],
+    'price': 15.00,
+    'currency': 'EUR',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ aos_analytic_stock_picking/models/stock_picking.py
@@ -0,0 +1,21 @@
+from odoo import models,fields,api,SUPERUSER_ID
+
+
+class StockMove(models.Model):
+    _inherit="stock.move"
+    
+    analytic_account_id = fields.Many2one('account.analytic.account')
+    
+    def _action_confirm(self, merge=None):
+        for rec in self :
+                rec.write(
+                    {"analytic_account_id" : rec.group_id.mrp_production_ids.analytic_account_id})
+        super(StockMove,self)._action_confirm() 
+  
+    
+    
+class StockPicking(models.Model):
+    _inherit ="stock.picking"
+    
+    analytic_account_id = fields.Many2one('account.analytic.account', related="move_lines.analytic_account_id")
+    
