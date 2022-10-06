b1c3b4c3f5ff7a8c4542dd1e7e8cb98f97e3d351
Ade <adeanshori@gmail.com>
2022-10-06 14:34:50 +0700

    update

--- aos_account_budget_alert_purchase/models/account_budget.py
+++ aos_account_budget_alert_purchase/models/account_budget.py
@@ -10,7 +10,7 @@ class BudgetLines(models.Model):
 
     def _compute_committed_amount(self):
         for line in self:
-            result = 0.0
+            result = result2 = 0.0
             acc_ids = line.general_budget_id.account_ids.ids
             if not acc_ids:
                 raise UserError(_("The Budget '%s' has no accounts!") % ustr(line.general_budget_id.name))
@@ -35,8 +35,8 @@ class BudgetLines(models.Model):
                 self.env.cr.execute(query, (line.analytic_account_id.id, date_from, date_to, acc_ids,))
                 result = self.env.cr.fetchone()[0] or 0.0
                 print ('===result===111===',result,line.analytic_account_id.id, date_from, date_to)
-                if result == 0.0:
-                    query = """
+                if result:
+                    query2 = """
                         SELECT SUM(aal.committed_amount)
                         FROM account_analytic_line aal
                         LEFT JOIN purchase_order_line pol ON aal.purchase_line_id=pol.id
@@ -48,8 +48,8 @@ class BudgetLines(models.Model):
                             AND po.state = 'purchase'
                             --AND aal.committed_locked = True
                         """
-                    self.env.cr.execute(query, (line.analytic_account_id.id, date_from, date_to,))
-                    result = self.env.cr.fetchone()[0] or 0.0
+                    self.env.cr.execute(query2, (line.analytic_account_id.id, date_from, date_to,))
+                    result2 = self.env.cr.fetchone()[0] or 0.0
                     #print ('-===query===-',query)
-                    print ('===result===222===',result,line.analytic_account_id.id, date_from, date_to)
-            line.committed_amount = result
+                    print ('===result===222===',result2,line.analytic_account_id.id, date_from, date_to)
+            line.committed_amount = result + result2
