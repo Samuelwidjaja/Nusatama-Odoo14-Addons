3123d7d300399ab7d6a8a2f016f7158dbed2ac0e
Imron Syabani <imoehsawa67@gmail.com>
2022-10-19 16:54:36 +0700

    required no po SO

--- aos_no_po_customer/models/sale_order.py
+++ aos_no_po_customer/models/sale_order.py
@@ -2,7 +2,7 @@
 - sale.order
 """
 from odoo import models, fields
-
+from odoo.exceptions import UserError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -10,4 +10,10 @@ class SaleOrder(models.Model):
 
 
 
-    no_po = fields.Char(string="No Pesanan")
+    no_po = fields.Char(string="No Pesanan",required=True)
+
+    def action_confirm(self):
+        if not self.no_po:
+            raise UserError("No Pesanan is required and cannot empty")
+
+        return super(SaleOrder,self).action_confirm()
