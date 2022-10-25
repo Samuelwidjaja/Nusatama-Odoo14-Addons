bb778280051b6bc9e4b66961c18243ac7a78f8a5
Aris <aris@portcities.net>
2022-10-25 12:53:02 +0700

    update

--- aos_draft_mo_so/__manifest__.py
+++ aos_draft_mo_so/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Aos Draft Mo From So',
+    'name' : 'Generate draft MO from Sales Order with MTO',
     'version': '14.0.0.1.0',
     'license': 'OPL-1',
-    'summary': 'Aos Draft Mo From So',
+    'summary': 'Generate draft MO from Sales Order with MTO by Alphasoft',
     'sequence': 1,
     "author": "Alphasoft",
     'description': """
-        Aos Draft Mo From So'
+        Generate draft MO from Sales Order with MTO'
     """,
     'category' : 'Tools',
     'website': 'https://www.alphasoft.co.id/',
@@ -22,7 +22,7 @@
     'qweb': [],
     'css': [],
     'js': [],
-    'price': 15.00,
+    'price': 10.00,
     'currency': 'EUR',
     'installable': True,
     'application': False,
--- aos_sales_auto_analytic/models/sale_order.py
+++ aos_sales_auto_analytic/models/sale_order.py
@@ -7,10 +7,10 @@ class SaleOrder(models.Model):
     # customer_reference = fields.Char()
     
     def action_confirm(self):
-        if not self.client_order_ref:
-            raise UserError('Customer Reference cannot empty and must be fill')
-
-        result = super(SaleOrder, self).action_confirm()
+        # if not self.client_order_ref:
+        #     raise UserError('Customer Reference cannot empty and must be fill')
+        #
+        # result = super(SaleOrder, self).action_confirm()
 
         if not self.analytic_account_id:
             self._saleorder_analytic_account_value()
