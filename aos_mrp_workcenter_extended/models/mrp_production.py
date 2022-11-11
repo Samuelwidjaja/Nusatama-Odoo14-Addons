d46d3bb11a0a17a1b77189a18b4aca5a95a8ff7f
ferdysaputra73 <ferdy_saputra73@gmail.com>
2022-11-11 16:58:02 +0700

    validation error on validate button mrp

--- aos_mrp_workcenter_extended/models/mrp_production.py
+++ aos_mrp_workcenter_extended/models/mrp_production.py
@@ -34,3 +34,21 @@ class MrpProduction(models.Model):
             production.workorder_ids = [(5, 0)] + [(0, 0, value) for value in workorders_values]
             for workorder in production.workorder_ids:
                 workorder.duration_expected = workorder._get_duration_expected()
+                
+    def button_mark_done(self):
+        res = super(MrpProduction, self).button_mark_done()
+        for rec in self.move_raw_ids :
+            if rec.reserved_availability < rec.product_uom_qty or rec.product_uom_qty > rec.reserved_availability :
+                raise UserError(_("To Consume Harus Sama Dengan Reserved Qty Pada Product %s") % rec.product_id.name)
+        return res
+    
+    #def write(self, vals):
+    #    res = super(MrpProduction, self).write(vals)
+    #    for rec in vals['move_raw_ids']:
+    #        num = 0
+    #        if rec[0] == 1 :
+    #            num += rec[1]
+    #            stockmove = self.env['stock.move'].browse(num)
+    #            if stockmove.workorder_id.state == 'done' and 'product_uom_qty' in rec[2]:
+    #                raise UserError(_("%s Already Finished Can't Change The Qty") % stockmove.workorder_id.name)
+    #    return res 
