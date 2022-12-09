2a3220c13947ed614ce3b1384016ec0ea8c71241
ferdysaputra73 <ferdy_saputra73@gmail.com>
2022-12-09 11:21:41 +0700

    update bom line

--- aos_mrp_bom/wizard/add_component.py
+++ aos_mrp_bom/wizard/add_component.py
@@ -15,74 +15,78 @@ class AddComponent(models.TransientModel):
           lines_bom = []
           lines_mrp =[]
           for rec in self.add_component : 
-                   # for prod in validate.move_raw_ids:
-                    if rec['box'] == True :
-                         if rec.product_id in list(mrp_production_obj.bom_id.bom_line_ids.product_id) :
-                              bom_recs = list(mrp_production_obj.bom_id.bom_line_ids.product_id)
-                              bom_new = rec.product_id
-                              bom_id = bom_recs.index(bom_new)
-                              id = mrp_production_obj.bom_id.bom_line_ids[bom_id].id
-                              lines_bom.append((1,id,{
-                                   "bom_id": mrp_production_obj.bom_id.id,
-                                   "product_id" : rec.product_id.id,
-                                   "product_uom_id":rec.product_id.uom_id.id,
-                                   "product_qty" : rec.product_qty + bom_recs[bom_id].bom_line_ids.product_qty
-                              }))
-                              lines_mrp.append((0,0,{
-                                   "product_id" : rec.product_id.id,
-                                   "name":rec.product_id.name,
-                                   "product_uom":rec.product_id.uom_id.id,
-                                   "location_id":mrp_production_obj.location_src_id.id,
-                                   "location_dest_id":rec.product_id.property_stock_production.id,
-                                   "product_uom_qty" : rec.product_qty,
-                                   "group_id" : mrp_production_obj.procurement_group_id.id,
-                                   "company_id" : mrp_production_obj.company_id.id,
-                                   "procure_method" : "make_to_order",
-                                   "warehouse_id": mrp_production_obj.move_raw_ids[0].warehouse_id.id,
-                                   "picking_type_id" : mrp_production_obj.picking_type_id.id,
-                        }))
-                         else :
-                              lines_bom.append((0,0,{
-                                   "bom_id": mrp_production_obj.bom_id.id,
-                                   "product_id" : rec.product_id.id,
-                                   "product_uom_id":rec.product_id.uom_id.id,
-                                   "product_qty" : rec.product_qty
-                              }))
-                              lines_mrp.append((0,0,{
-                                   "product_id" : rec.product_id.id,
-                                   "name":rec.product_id.name,
-                                   "product_uom":rec.product_id.uom_id.id,
-                                   "location_id":mrp_production_obj.location_src_id.id,
-                                   "location_dest_id":rec.product_id.property_stock_production.id,
-                                   "product_uom_qty" : rec.product_qty,
-                                   "group_id" : mrp_production_obj.procurement_group_id.id,
-                                   "company_id" : mrp_production_obj.company_id.id,
-                                   "procure_method" : "make_to_order",
-                                   "warehouse_id": mrp_production_obj.move_raw_ids[0].warehouse_id.id,
-                                   "picking_type_id" : mrp_production_obj.picking_type_id.id,
-                        }))
-                    else :      
+               if rec['box'] == True :
+               #      if rec.product_id.id in mrp_production_obj.bom_id.bom_line_ids.product_id.ids :
+               #           bom_recs = list(mrp_production_obj.bom_id.bom_line_ids.product_id)
+               #           # bom_new = rec.product_id
+               #           # bom_id = bom_recs.index(rec.product_id)
+               #           # id = mrp_production_obj.bom_id.bom_line_ids[bom_id].id
+               #           lines_bom.append((0,0,{
+               #                "bom_id": mrp_production_obj.bom_id.id,
+               #                "product_id" : rec.product_id.id,
+               #                "product_uom_id":rec.product_id.uom_id.id,
+               #                "product_qty" : rec.product_qty 
+               #           }))
+               #           # if len(bom_recs[bom_id].bom_line_ids) != 1 :
+               #           #      for rec in bom_recs[bom_id].bom_line_ids
+               #           #           if rec.id != id :
+               #           #           mrp_production_obj.bom_id.write({"bom_line_ids" :newrec})
+               #           #      bom_recs[bom_id].bom_line_ids = bom_recs[bom_id].bom_line_ids[0]
+               #           lines_mrp.append((0,0,{
+               #                "product_id" : rec.product_id.id,
+               #                "name":rec.product_id.name,
+               #                "product_uom":rec.product_id.uom_id.id,
+               #                "location_id":mrp_production_obj.location_src_id.id,
+               #                "location_dest_id":rec.product_id.property_stock_production.id,
+               #                "product_uom_qty" : rec.product_qty,
+               #                "group_id" : mrp_production_obj.procurement_group_id.id,
+               #                "company_id" : mrp_production_obj.company_id.id,
+               #                "procure_method" : "make_to_order",
+               #                "warehouse_id": mrp_production_obj.move_raw_ids[0].warehouse_id.id,
+               #                "picking_type_id" : mrp_production_obj.picking_type_id.id,
+               #     }))
+               #      else :
+                         lines_bom.append((0,0,{
+                              "bom_id": mrp_production_obj.bom_id.id,
+                              "product_id" : rec.product_id.id,
+                              "product_uom_id":rec.product_id.uom_id.id,
+                              "product_qty" : rec.product_qty
+                         }))
                          lines_mrp.append((0,0,{
-                             "product_id" : rec.product_id.id,
-                             "name":rec.product_id.name,
-                             "product_uom":rec.product_id.uom_id.id,
-                             "location_id":mrp_production_obj.location_src_id.id,
-                             "location_dest_id":rec.product_id.property_stock_production.id,
-                             "product_uom_qty" : rec.product_qty,
-                             "group_id" : mrp_production_obj.procurement_group_id.id,
-                             "company_id" : mrp_production_obj.company_id.id,
-                             "warehouse_id": mrp_production_obj.move_raw_ids[0].warehouse_id.id,
-                             "procure_method" : "make_to_order",
-                             "picking_type_id" : mrp_production_obj.picking_type_id.id,
-                        }))
+                              "product_id" : rec.product_id.id,
+                              "name":rec.product_id.name,
+                              "product_uom":rec.product_id.uom_id.id,
+                              "location_id":mrp_production_obj.location_src_id.id,
+                              "location_dest_id":rec.product_id.property_stock_production.id,
+                              "product_uom_qty" : rec.product_qty,
+                              "group_id" : mrp_production_obj.procurement_group_id.id,
+                              "company_id" : mrp_production_obj.company_id.id,
+                              "procure_method" : "make_to_order",
+                              "warehouse_id": mrp_production_obj.move_raw_ids[0].warehouse_id.id,
+                              "picking_type_id" : mrp_production_obj.picking_type_id.id,
+                   }))
+               else :      
+                    lines_mrp.append((0,0,{
+                        "product_id" : rec.product_id.id,
+                        "name":rec.product_id.name,
+                        "product_uom":rec.product_id.uom_id.id,
+                        "location_id":mrp_production_obj.location_src_id.id,
+                        "location_dest_id":rec.product_id.property_stock_production.id,
+                        "product_uom_qty" : rec.product_qty,
+                        "group_id" : mrp_production_obj.procurement_group_id.id,
+                        "company_id" : mrp_production_obj.company_id.id,
+                        "warehouse_id": mrp_production_obj.move_raw_ids[0].warehouse_id.id,
+                        "procure_method" : "make_to_order",
+                        "picking_type_id" : mrp_production_obj.picking_type_id.id,
+                   }))
           if lines_bom:
-               for item in lines_bom :
-                    tuprec = tuple(item)
-                    newrec = [tuprec]
-                    if item[2]['product_id'] in list(mrp_production_obj.bom_id.bom_line_ids.product_id.ids) :
-                         add_bom = mrp_production_obj.bom_id.write({"bom_line_ids" :newrec})
-                    else:
-                         add_bom = mrp_production_obj.bom_id.update({"bom_line_ids" :newrec})
+               # for item in lines_bom :
+               #      tuprec = tuple(item)
+               #      newrec = [tuprec]
+               #      if item[2]['product_id'] in list(mrp_production_obj.bom_id.bom_line_ids.product_id.ids) :
+               #           add_bom = mrp_production_obj.bom_id.update({"bom_line_ids" :newrec})
+               #      else:
+               add_bom = mrp_production_obj.bom_id.update({"bom_line_ids" :lines_bom})
                if mrp_production_obj.state != 'draft':
                     mrp_line = mrp_production_obj.update({"move_raw_ids" :lines_mrp})
                     mrp_production_obj.move_raw_ids._action_confirm()
