bc3752c394ed4ee1a3ac6ce01dc83fb6a83a965c
imronsyabani <imoehsawa67@gmail.com>
2023-11-24 15:18:16 +0700

    update float to hour

--- aos_mrp_labour_foh/models/__init__.py
+++ aos_mrp_labour_foh/models/__init__.py
@@ -3,8 +3,7 @@
 
 def float_to_hour(duration):
     """ Convert a number of hours into a time object. """
-    hours, seconds = divmod(duration * 60, 3600)  # split to hours and seconds
-    minutes, seconds = divmod(seconds, 60)  # split the seconds to minutes and seconds
+    hours, minutes = divmod(duration, 60)
     return hours + (minutes / 100.0) # handle zero hour
 
 from . import account_account
