a32ee01e36a79b30082717fb752968d4d02c6f73
imronsyabani <imoehsawa67@gmail.com>
2023-12-08 10:08:21 +0700

    update float to time

--- aos_mrp_labour_foh/models/__init__.py
+++ aos_mrp_labour_foh/models/__init__.py
@@ -1,10 +1,11 @@
 
-
+from datetime import datetime, timedelta
 
 def float_to_hour(duration):
     """ Convert a number of hours into a time object. """
-    hours = duration / 60.0
-    return hours # handle zero hour
+    td = timedelta(minutes=duration).total_seconds()
+    hours = td / 3600.0
+    return hours # handle zero hour 12.38, 0.20633333333333334, 0.21
 
 from . import account_account
 from . import mrp_labour_foh
