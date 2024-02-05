f96a04e4d6cc2e0ebbb80753caa396c8c64f1c37
imronsyabani <imoehsawa67@gmail.com>
2024-02-05 12:10:57 +0700

    update mrp timesheet & accunt analytic

--- aos_accounting/models/accounting_vendor.py
+++ aos_accounting/models/accounting_vendor.py
@@ -14,5 +14,23 @@ class AccountingVendor(models.Model):
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
+    
     mrp_id = fields.Char(string="MO ID " ,related="move_id.mrp_id")
     
+    
+    # 
+
+class AccountAnalyticDefault(models.Model):
+    _inherit = "account.analytic.default"
+    
+    # inherit existing method
+    # do not add analytic account if account type is expense
+    @api.model
+    def account_get(self, product_id=None, partner_id=None, account_id=None, user_id=None, date=None, company_id=None):
+        account_type_id = self.env.ref('account.data_account_type_expenses', raise_if_not_found=False)
+        
+        account = self.env['account.account'].browse(account_id)
+        if not account or account.user_type_id.id == account_type_id.id:
+            return self.env['account.analytic.default']
+
+        return super(AccountAnalyticDefault, self).account_get(product_id, partner_id, account_id, user_id, date, company_id)
